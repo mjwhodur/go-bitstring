e56e955c600f70afba1450071b87a138f9e78717
Michal Hodur <michal@kolektyw.io>
2020-11-06 19:19:37 +0100

    Add telecommunication function stubs

--- bitstring.go
+++ bitstring.go
@@ -1,9 +1,12 @@
 package bitstring
 
+import "bytes"
+
 type BitString struct {
 	BitArray []uint8
 }
 
+//
 func (bs *BitString) AsString() string {
 	return string(bs.BitArray)
 }
@@ -16,6 +19,19 @@ func (bs *BitString) Uint(start int, end int) int {
 	return 0
 }
 
-func (bs *BitString) ICID(start int, end int) string {
-	return "XXX"
+func (bs *BitString) ICID(start int, end int) (error, string) {
+	return nil, ""
 }
+
+func (bs *BitString) IBCD(start int, end int) (error, string) {
+	return nil, ""
+}
+
+func FromBytes(bytes []rune) (error, BitString) {
+	return nil, BitString{}
+}
+
+func FromStream(buffer bytes.Buffer) (error, BitString) {
+	return nil, BitString{}
+}
+
