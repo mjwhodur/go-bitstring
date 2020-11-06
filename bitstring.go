3fb2252e814e02499c395c970aedef21184653eb
Michal Hodur <michal@michal.dev>
2020-11-06 20:28:05 +0100

    [main] Added functions: ByteToBitArray, altered other functions.

--- bitstring.go
+++ bitstring.go
@@ -1,7 +1,12 @@
+// Package bitstring provides functions for parsing and manipulation of
+//
 package bitstring
 
-import "bytes"
-
+import (
+	"bytes"
+	"math"
+)
+// Base BitString structure consisting of bytes representing
 type BitString struct {
 	BitArray []uint8
 }
@@ -11,27 +16,71 @@ func (bs *BitString) AsString() string {
 	return string(bs.BitArray)
 }
 
-func (bs *BitString) Length() int {
+func (bs *BitString) BitLength() int {
      return len(bs.BitArray)
 }
 
-func (bs *BitString) Uint(start int, end int) int {
+func (bs *BitString) BitStringToUint(start int, end int) int {
 	return 0
 }
 
-func (bs *BitString) ICID(start int, end int) (error, string) {
+func (bs *BitString) BitStringToICID(start int, end int) (error, string) {
 	return nil, ""
 }
 
-func (bs *BitString) IBCD(start int, end int) (error, string) {
+func (bs *BitString) BitStringToIBCD(start int, end int) (error, string) {
 	return nil, ""
 }
 
-func FromBytes(bytes []rune) (error, BitString) {
-	return nil, BitString{}
+// Telephony Binary Coded Decimal (TBCD) helper function provide assistance in
+//converting TBCD strings from BitString slice form as in standard ITU-T Q.825.
+func (bs *BitString) BitStringToTBCD(start int, end int) (error, string) {
+	return nil, ""
 }
 
+// Appends byte to BitArray as bits
+func (bs *BitString) appendByte(rune byte) error {
+	for _, bitVal := range ByteToBitArray(rune).BitArray {
+		bs.BitArray = append(bs.BitArray, bitVal)
+	}
+
+ return nil
+}
+
+// Creates BitString struct from byte slice.
+func FromBytes(bytes []byte) (error, BitString) {
+	bs := BitString{}
+	for _, rune := range(bytes) {
+		err := bs.appendByte(rune)
+		if err != nil {
+			return err, BitString{}
+		}
+	}
+	return nil, bs
+}
+
+// Creates BitString struct from buffered data.
 func FromStream(buffer bytes.Buffer) (error, BitString) {
 	return nil, BitString{}
+}
+
+func ByteToBitArray(b byte) BitString {
+	var revbitSlice []uint8
+	var bitSlice []uint8
+	for x:=0; x < 8; x++ {
+		c := byte(math.Pow(2, float64(x)))
+		if c < b {
+			b = b - c
+			revbitSlice = append(revbitSlice, 1)
+
+		} else {
+			revbitSlice = append(revbitSlice, 0)
+		}
+	}
+
+	for i := len(revbitSlice) - 1; i == 0; i-- {
+		bitSlice = append(bitSlice, revbitSlice[i])
+	}
+	return BitString{BitArray: bitSlice}
 }
 
